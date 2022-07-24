4baf3c1f5e7dac49c2272dc229be79a0f89ac56d
Mina Ghaly <minanabil@msn.com>
2022-07-24 20:42:50 +1000

    all sections fixed and a Readme file is generated to match the requirements

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -12,7 +12,7 @@ function renderLicenseBadge(license) {
 function renderLicenseLink(license) {
   if (license == "None"){return ""}
   else{
-    
+
   }
 }
 
@@ -20,7 +20,10 @@ function renderLicenseLink(license) {
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license == "None"){ return ""}
+  else{ return license.licenseType}
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
@@ -52,7 +55,7 @@ ${data.usageInformation}
 ${data.contributionGuidelines}
 
 ## License
-${data.licenseType}
+${renderLicenseSection(data.licenseType)}
 
 ## Test Instructions
 ${data.testInstructions}
--- README.md
+++ README.md
@@ -26,7 +26,7 @@
 
 
 ## License
-None
+
 
 ## Test Instructions
 
