1def9f2ed0ddd964e9a541eb229e70cc6c942991
Mina Ghaly <minanabil@msn.com>
2022-07-24 22:01:33 +1000

    readme file for the project

--- Develop/index.js
+++ Develop/index.js
@@ -2,6 +2,9 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 const util = require("../Develop/utils/generateMarkdown");
+const { defaultMaxListeners } = require("events");
+const { builtinModules } = require("module");
+const { NONAME } = require("dns");
 
 // TODO: Create an array of questions for user input
 const questions = [{
@@ -67,3 +70,23 @@ function init() {
 
 // Function call to initialize app
 init();
+
+
+
+// Readme file
+// this project is called README creator. It runs on Node.js and does not require a browser.
+// It helps software developpers create a readme file in markdown where the user once run the PerformanceNavigationTiming,
+// he is asked 9 questions which are title of my project, Installation instructions,usage information, 
+// contribution guidelines, test instructions, license type, github username  and email address.
+
+// Once all questions are answered, a README.md file is created that has all these details.
+
+// The program uses 2 Node.js modules, the inquirer module that is used to prompt user 
+// with questions and the fs module that creates a README file and  write the users answers in this
+// file. One of the questions as seen above is the license type of the program where the user is given
+// 3 license types and allowed to choose between them or a fourth of license to be None.
+
+// If a license is choosen, a badge is added to the top of the readme file and if no license,
+// of course no badge is added. The readme file as well has a table of contents with a link to 
+// every section in the file to make it easier for the user to navigate through the sections
+// by clicking on the links in the readme file.
--- README.md
+++ README.md
@@ -1,11 +1,11 @@
-# 
+# MAR 
 
-
+![This is an image](https://img.shields.io/badge/license-Apache%202.0-brightgreen)
 
 
 
 ## Description
-
+Youtube top 10 displayer
 
 ## Table of Contents
 - [Installation instructions](#installation-instructions)
@@ -17,26 +17,26 @@
 
 
 ## Installation instructions 
-
+Does not require, just a live Github link
 
 ## Usage Information
-
+Can be run on chrome, edge and any browser
 
 ## Contribution Guidelines
-
+its an open source
 
 ## License
-None
+Apache 2.0
 
 ## Test Instructions
-
+please make sure to test it on new versions of chrome
 
 ## Questions
--GitHub username:<br><br>
+-GitHub username:<br>Master20100<br>
 -GitHub profile<br>
-https://github.com/<br>
+https://github.com/Master20100<br>
 For any questions, please reach me on my email address as below <br>
 -Email address<br>
-<>
+<minanabil@msn.com>
 
   
